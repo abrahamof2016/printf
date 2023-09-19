d2fc838f2405495b249bc7707b518ecd2581de8a
jarageda <jarageda9@gmail.com>
2023-09-19 19:19:25 +0000

    i have deleted the file called _printf.c

---
+++ test/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - test function.
+  * Return: an integer.
+  */
+int main(void)
+{
+	_printf("Hello this is %c\n", 'a');
+	printf("Hello this is %c\n", 'b');
+	return (0);
+}
