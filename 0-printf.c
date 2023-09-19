bb2067a9c3f85f134cb6a3d6f051bb85633c9abf
jarageda <jarageda9@gmail.com>
2023-09-19 19:07:37 +0000

    custom printf function

--- 0-printf.c
+++ 0-printf.c
@@ -1,36 +1,53 @@
 #include "main.h"
+void print_buffer(char buffer[], int *buff_ind);
 /**
-  * _printf: writes a string and character to stdout.
-  * @format: format string.
-  * Return: 0 on success.
-  *
-  */
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	int i;
-	va_list args;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
 	char buffer[BUFF_SIZE];
-
-	i = 0;
 	if (format == NULL)
 		return (-1);
-	va_start(args, format);
+	va_start(list, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			buffer[i] = format[i];
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
 		}
-		if (format[i] == '%')
+		else
 		{
-			format++;
-			if (*format == 'c')
-				buffer[i] = va_arg(args, int);
-
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
-		i++;
 	}
-	va_end(args);
-	write(1, &buffer[0], strlen(buffer));
-	return (0);
-}
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_chars);
+	/**
+	 * print_buffer - Prints the contents of the buffer if it exist
+	 * @buffer: Array of chars
+	 * @buff_ind: Index at which to add next char, represents the length.
+	 */
+	void print_buffer(char buffer[], int *buff_ind)
+	{
+		if (*buff_ind > 0)
+			write(1, &buffer[0], *buff_ind);
+		*buff_ind = 0;
+	}
