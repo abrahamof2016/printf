29d36e6c25af3cfd54c144ede1d4cf356a1be0f0
jarageda <jarageda9@gmail.com>
2023-09-20 00:39:36 +0000

    writes to stdout

---
+++ 0-printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _printf - writes a character and string to stdout.
+ * @format: format string.
+ * Return: 0 on success.
+ **/
+int _printf(const char *format, ...)
+{
+	int i;
+	va_list args;
+	char *buffer = malloc(strlen(format) + 1);
+
+	if (format == NULL || buffer == NULL)
+		write(1, "Invalid", 7);
+	i = 0;
+	va_start(args, format);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[i] = format[i];
+		}
+		else if (format[i] == '%')
+		{
+			++format;
+			if (format[i] == 'c')
+			{
+				int val = va_arg(args, int);
+
+				if ((val > -128) && (val < 127))
+					buffer[i] = val;
+			}
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				if (str != NULL)
+				{
+					strncpy(&buffer[i], str, strlen(str));
+					i += strlen(str);
+				}
+			}
+		}
+	}
+	va_end(args);
+	write(1, &buffer[0], strlen(buffer));
+	free(buffer);
+	return (0);
+}
