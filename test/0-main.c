e66fd249da9ba53604f45a8f37f70086264762b3
jarageda <jarageda9@gmail.com>
2023-09-20 00:25:15 +0000

    0-main.c file deleted

--- test/0-main.c
+++ test/0-main.c
@@ -1,5 +1,3 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 /**
   * main - Entry point.
@@ -14,7 +12,7 @@ int main(void)
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
+	ui = (unsigned int) INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
