e291881e4d337a9dbd136b315acf73a186718fa9
jarageda <139215020+jarageda@users.noreply.github.com>
2023-09-22 21:32:33 +0300

    Update handle.c

--- handle.c
+++ handle.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
-  * handle_print - handles a format specifiers.
-  * @fmt: a format string.
-  * @list: list of arguments to be printed.
-  * @ind: an integer counter.
-  * @buffer: a buffer array.
-  * @flags: number of flags.
-  * @width: width.
-  * @precision: specifies a specification.
-  * @size: size.
-  * Return: an integer.
-  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,10 +25,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
 	if (fmt_types[i].fmt == '\0')
-	{
+{
 		if (fmt[*ind] == '\0')
-			return  (-1);
+			return (-1);
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
@@ -40,9 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int fla
 				--(*ind);
 			return (1);
 		}
-		unknow_len += write (1, &fmt[*ind], 1);
+		unknow_len += write(1, &fmt[*ind], 1);
 		return (unknow_len);
 	}
 	return (printed_chars);
 }
-
