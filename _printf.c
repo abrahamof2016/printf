d4e24407915c35fa4db62bd3e0e085b9165005f1
jarageda <jarageda9@gmail.com>
2023-09-22 19:22:16 +0000

    custom printf

--- _printf.c
+++ _printf.c
@@ -47,3 +47,14 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (printed_chars);
 }
+/**
+  * print_buffer - prints the contents of the buffer if it exits.
+  * @buffer: Array of chars.
+  * @buff_ind: index at which to add next char.
+  */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
+}
